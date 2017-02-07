33a75dff79e303f90cf7f4fe2abb1602ba802751
Adam Harper <github@adam-harper.com>
2017-02-07 08:25:06 +0000

    Only look up parts whose status permits demands

--- src/cefu/queries/parts.sql
+++ src/cefu/queries/parts.sql
@@ -1,8 +1,15 @@
 select
-  'LPE' as contract,
-  max(part_no) as id,
-  cust_part_no as "number"
-from ifsinfo.inv_part_cust_part_no
+  ip.contract,
+  max(ipcp.part_no) as id,
+  ipcp.cust_part_no as "number"
+from ifsinfo.inv_part_cust_part_no ipcp
+join ifsapp.inventory_part ip
+  on ip.contract = 'LPE'
+  and ipcp.part_no = ip.part_no
+join ifsapp.inventory_part_status_par ps
+  on ip.part_status = ps.part_status
 where cust_part_no is not null
+  and ps.demand_flag_db = 'Y'
 group by
-  cust_part_no
+  ip.contract,
+  ipcp.cust_part_no
